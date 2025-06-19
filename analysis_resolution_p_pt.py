6fc588a0ee629e06075ceb2a67ca0579e282e2d2
Jesus-A-Galarza <71800508+Jesus-A-Galarza@users.noreply.github.com>
2025-06-18 23:12:38 -0400

    Update analysis_resolution_p_pt.py

--- analysis_resolution_p_pt.py
+++ analysis_resolution_p_pt.py
@@ -10,17 +10,16 @@ ROOT.gROOT.SetBatch()
 ROOT.gStyle.SetOptStat(0)
 ROOT.gStyle.SetOptTitle(0)
 
-ROOT.EnableImplicitMT()  # use all cores
+ROOT.EnableImplicitMT()
 
-# load libraries
 ROOT.gSystem.Load("libFCCAnalyses")
 fcc_loaded = ROOT.dummyLoader()
 ROOT.gInterpreter.Declare("using namespace FCCAnalyses;")
 ROOT.gInterpreter.Declare('#include "functions.h"')
 
-# Declare custom transverse momentum resolution function
+# Declare inverse momentum resolution function
 ROOT.gInterpreter.Declare("""
-ROOT::VecOps::RVec<float> leptonResolutionPt(
+ROOT::VecOps::RVec<float> leptonResolutionInvP(
     const ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData>& reco_muons,
     const ROOT::VecOps::RVec<int>& reco_match_idx,
     const ROOT::VecOps::RVec<int>& mc_match_idx,
@@ -59,25 +58,25 @@ ROOT::VecOps::RVec<float> leptonResolutionPt(
         }
 
         const auto& mc = allMC[mc_idx];
-        float reco_pt = std::sqrt(reco.momentum.x * reco.momentum.x + reco.momentum.y * reco.momentum.y);
-        float mc_pt = std::sqrt(mc.momentum.x * mc.momentum.x + mc.momentum.y * mc.momentum.y);
+        float reco_p = std::sqrt(reco.momentum.x * reco.momentum.x + reco.momentum.y * reco.momentum.y + reco.momentum.z * reco.momentum.z);
+        float mc_p   = std::sqrt(mc.momentum.x * mc.momentum.x + mc.momentum.y * mc.momentum.y + mc.momentum.z * mc.momentum.z);
 
-        if (mc_pt < 1e-3) {
+        if (mc_p < 1e-3 || reco_p < 1e-3) {
             resolution.push_back(-998.);
             continue;
         }
 
-        resolution.push_back((reco_pt - mc_pt) / mc_pt);
+        resolution.push_back(1.0f / reco_p - 1.0f / mc_p);
     }
 
     return resolution;
 }
 """)
 
-# Histogram binning
 bins_p = (250, 0, 250)
 bins_pt = (250, 0, 250)
 bins_res = (10000, -0.05, 0.05)
+
 
 def analysis(input_files, output_file):
     df = ROOT.RDataFrame("events", input_files)
@@ -88,32 +87,25 @@ def analysis(input_files, output_file):
 
     df = df.Define("muons_all", "FCCAnalyses::ReconstructedParticle::get(Muons, ReconstructedParticles)")
 
-    # Total and transverse momentum
     df = df.Define("muons_p", "FCCAnalyses::ReconstructedParticle::get_p(muons_all)")
     df = df.Define("muons_pt", "FCCAnalyses::ReconstructedParticle::get_pt(muons_all)")
     muons_p = df.Histo1D(("muons_p", "", *bins_p), "muons_p")
     muons_pt = df.Histo1D(("muons_pt", "", *bins_pt), "muons_pt")
 
-    # Resolution in total momentum
-    df = df.Define("muons_res_p", "FCCAnalyses::leptonResolution(muons_all, MCRecoAssociations0, MCRecoAssociations1, ReconstructedParticles, Particle, 0)")
-    muons_res_p = df.Histo1D(("muons_res_p", "", *bins_res), "muons_res_p")
+    df = df.Define("muons_res_invp", "leptonResolutionInvP(muons_all, MCRecoAssociations0, MCRecoAssociations1, ReconstructedParticles, Particle, 0)")
+    df = df.Filter("All(muons_res_invp > -998)", "Filter invalid invp resolutions")
+    df = df.Filter("All(abs(muons_res_invp) < 0.1)", "Filter extreme invp resolution values")
+    muons_res_invp = df.Histo1D(("muons_res_invp", "", 10000, -0.01, 0.01), "muons_res_invp")
 
-    # Resolution in transverse momentum
-    df = df.Define("muons_res_pt", "leptonResolutionPt(muons_all, MCRecoAssociations0, MCRecoAssociations1, ReconstructedParticles, Particle, 0)")
-    # df = df.Filter("All(muons_res_pt > -998)", "Filter invalid pt resolutions")
-    # df = df.Filter("All(abs(muons_res_pt) < 1.5)", "Filter extreme pt resolution values")
-    muons_res_pt = df.Histo1D(("muons_res_pt", "", *bins_res), "muons_res_pt")
-
-    # Output
     fout = ROOT.TFile(output_file, "RECREATE")
     muons_p.Write()
     muons_pt.Write()
-    muons_res_p.Write()
-    muons_res_pt.Write()
+    muons_res_invp.Write()
     fout.Close()
 
+
 if __name__ == "__main__":
-    input_files, output_file = ["samples/CLD_1.0T_Zmumu_ecm240.root"], "output/CLD_1.0T_Zmumu_ecm240_p_pt.root"
+    input_files, output_file = ["samples/CLD_1T_Zmumu_ecm240.root"], "output/CLD_1T_Zmumu_ecm240_invp_res.root"
     logger.info(f"Start analysis")
     analysis(input_files, output_file)
     logger.info(f"Done! Output saved to {output_file}")
